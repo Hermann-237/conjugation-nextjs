5c818d23194d21d1c3f05f8fee46de20b0623e65
Hermann Tamouya Totim <h.totim@eclever.io>
2021-12-30 22:50:42 +0100

    add the cookies to the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,13 @@ import styled from '@emotion/styled'
 import Naviation from '../components/Navigation'
 import React from 'react'
 import Footer from '../components/Footer'
+import Cookies from 'js-cookie'
+import CookieConsent from "react-cookie-consent"
+
 
 const Base = styled.div(tw`min-height[94vh] mx-4 big:mx-20`)
 function MyApp({ Component, pageProps }: AppProps) {
+  const getCookies = Cookies.get('simplecyto')
   return (
     <>
       <Naviation />
@@ -15,6 +19,25 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Base>
       <Footer />
+      {
+         !getCookies && (<CookieConsent
+            debug={true}
+            location={'bottom'}
+            style={{ background: "#292b2c", height: "200px", paddingTop: "15px", display: "flex", flexDirection: "row" }}
+            cookieName="simplecyto"
+            cookieValue={true}
+            buttonText="I agree"
+            expires={365}
+            ariaAcceptLabel={'true'}
+            buttonClasses="bg-info text-white px-5 ml-5 mt-sm-1"
+            containerClasses="d-flex justify-content-center"
+            contentClasses="col-12"
+            overlay
+            onAccept={()=>Cookies.set('simplecyto','true')}
+          >
+            <p>Cookies allow us to provide our services. By using our services, you agree to our use of cookies. See our  <a href="/privacy-policy" className="text-warning"> privacy policy </a>for more </p>
+          </CookieConsent>)
+        }
     </>
   )
 }
