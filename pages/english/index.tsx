42bd1778858a92da86cae3e5c3d5064003e3ec67
Hermann Tamouya Totim <h.totim@eclever.io>
2021-12-30 23:31:38 +0100

    add the cookies to the website

--- pages/english/index.tsx
+++ pages/english/index.tsx
@@ -1,13 +1,9 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import Header from '../../components/Header'
-import tw from 'twin.macro'
-import styled from '@emotion/styled'
 import { EnglishMessages } from '../../messages/EnglishMessages'
-import {useRouter} from 'next/router'
 
 
 const EnglishIndex = () => {
-  const router = useRouter()
   return (
     <>
       <Header
